1ca44c57b59777c35aec3685009c6a04bb7f4669
Camille TOURON <camille.touron.pro@gmail.com>
2023-05-10 23:14:11 +0200

    correction cors

--- server.js
+++ server.js
@@ -18,7 +18,16 @@ const passServer = (req, res, next) => {
 };
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "null");
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "null",
+    "*",
+    "http://localhost",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.header("Access-Control-Allow-Credentials", "true");
 
   res.header(
--- test.js
+++ test.js
@@ -23,3 +23,29 @@ xhr.setRequestHeader("Content-Type", "application/json");
 
 xhr.send(data);
 //156.67.217.63
+var myHeaders = new Headers();
+myHeaders.append(
+  "token",
+  "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInVzZXJNYWlsIjoiY2FtaWxsZS50b3Vyb24ucHJvQGdtYWlsLmNvbSIsImlzQWRtaW4iOmZhbHNlLCJleHAiOjE2ODM3MTIwMTB9.qfM8iHCdT9CMZ7RwiZWmjh37Zmo7O8MJKBqG8LsBX4I"
+);
+myHeaders.append("mail", "camille.touron.pro@gmail.com");
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  mail_from: "camille.touron.pro@gmail.com",
+  mail_to: "camille.touron.pro@gmail.com",
+  message: "Hello, Bob!",
+});
+
+var requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow",
+  mode: "cors",
+};
+
+fetch("http://156.67.217.63:3000/messages", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.log("error", error));
