16892754269ac1cabcbb9f98acab05318fdb1106
Camille TOURON <camille.touron.pro@gmail.com>
2023-05-10 09:42:02 +0200

    correction cors

--- server.js
+++ server.js
@@ -17,14 +17,7 @@ const passServer = (req, res, next) => {
   next();
 };
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 app.use("/messages", passServer, messageRouter);
