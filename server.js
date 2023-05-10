a1499f3db44ab340e73c533dc3f263e6a1c2d0ff
Camille TOURON <camille.touron.pro@gmail.com>
2023-05-10 09:32:44 +0200

    Correction cors

--- server.js
+++ server.js
@@ -17,15 +17,13 @@ const passServer = (req, res, next) => {
   next();
 };
 
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,PUT,POST,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-    exposedHeaders: "Content-Length,Authorization",
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.use(express.static("public"));
 app.use(express.json());
