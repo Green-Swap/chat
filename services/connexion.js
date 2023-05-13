64ddc39b0891ed14e4db4711afd22e3e341539d2
Camille TOURON <camille.touron.pro@gmail.com>
2023-05-13 12:23:56 +0200

    changement connexion en singleton

--- services/connexion.js
+++ services/connexion.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/chat";
 
-exports.connexion = function () {
-  mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+let connection;
+
+exports.getConnection = function () {
+  if (!connection) {
+    connection = mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+  return connection;
 };
