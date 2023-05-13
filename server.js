abcef4376e9bf63e4845925b52e7f6e17de7e88c
Camille TOURON <camille.touron.pro@gmail.com>
2023-05-13 16:48:27 +0200

    test

--- server.js
+++ server.js
@@ -44,7 +44,6 @@ app.use("/messages", passServer, messageRouter);
 
 io.on("connection", (socket) => {
   console.log("A user connected");
-
   socket.on("disconnect", () => {
     console.log("A user disconnected");
   });
